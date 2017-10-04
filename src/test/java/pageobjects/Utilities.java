ec01ea67f0a0afc17905f6d665642ef7bcca377b
syam.s@outlook.com <Liverp00l>
2017-10-04 08:59:28 +0200

    Updated Utilities

--- src/test/java/pageobjects/Utilities.java
+++ src/test/java/pageobjects/Utilities.java
@@ -1,6 +1,50 @@
 package pageobjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
 public class Utilities {
+    private WebDriver driver;
+
+    @BeforeClass
+    @Parameters("browser")
+
+    public void setUp(String browser){
+
+
+
+         if (browser.equalsIgnoreCase("Chrome"))
+
+        {
+            System.setProperty("webdriver.chrome.driver", "C:/BrowserUtilities/chromedriver.exe");
+            driver = new ChromeDriver();
+            System.out.println("inside the general");
+
+        }
+        else if (browser.equalsIgnoreCase("IE"))
+
+        {
+          /*  DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+            caps.setCapability("ignoreZoomSetting", true);*/
+
+            System.setProperty("webdriver.ie.driver", "C:/BrowserUtilities/IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+
+
+        }
+        else if (browser.equalsIgnoreCase("Firefox")){
+            System.setProperty("webdriver.gecko.driver","C:/BrowserUtilities/geckodriver.exe");
+
+            FirefoxDriver driver = new FirefoxDriver();
+
+
+        }
+
+    }
 
 
 }
