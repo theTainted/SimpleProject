1803ed6b6c14d39a67d09378ae6696945165490c
syam.s@outlook.com <Liverp00l>
2017-10-04 08:31:57 +0200

    added the testng xml added an empty class Utilities for creating proper multibrowser execution

--- src/test/java/testcases/TrialTestClass.java
+++ src/test/java/testcases/TrialTestClass.java
@@ -1,4 +1,17 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class TrialTestClass {
+    @Test
+    public void testTrialTestClass(){
+        WebDriver driver;
+
+        System.setProperty("webdriver.chrome.driver", "C:/BrowserUtilities/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+    }
 }
