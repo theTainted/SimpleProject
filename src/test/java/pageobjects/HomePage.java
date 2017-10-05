2373450a9b2dfca04a80330020ea96a8a4ea68ad
syam.s@outlook.com <Liverp00l>
2017-10-05 08:32:30 +0200

    still getting  a null pointer even after making the utilities class static.

--- src/test/java/pageobjects/HomePage.java
+++ src/test/java/pageobjects/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends Utilities {
-    //Utilities utilities = new Utilities();
+    Utilities utilities = new Utilities();
 
     @FindBy(xpath = "//button/span[@class='ui-button-icon-primary ui-icon ui-icon-closethick']")
     WebElement btnCloseNewsLetter;
