93d3d912c35c0c1b14a7a056c3afcc6b67b6e6a5
syam.s@outlook.com <Liverp00l>
2017-10-10 10:50:30 +0200

    Updated the testng.xml to have one more testclass.

--- src/test/java/testcases/CoretfielSearch.java
+++ src/test/java/testcases/CoretfielSearch.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -58,4 +59,10 @@ public class CoretfielSearch {
         homePage.enterSearchTextAndSearch("rAincoat");
 
     }
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+
 }
