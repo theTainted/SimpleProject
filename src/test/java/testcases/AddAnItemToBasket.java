39efab3cd9ebcb2d24b0114e8d7676ba1daffa93
syam.s@outlook.com <Liverp00l>
2017-10-10 10:47:18 +0200

    Created the class AddAnItemToBasket. Right now its only till the Search. Using the search by passing an exact SKUID, so the user lands in the PDP

---
+++ src/test/java/testcases/AddAnItemToBasket.java
@@ -0,0 +1,61 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pageobjects.HomePage;
+
+public class AddAnItemToBasket {
+    public static WebDriver driver; // changed to static
+    public String sURL = "https://" + "storefront" + ":" + "storefront" + "@" + "staging.cortefiel.com/es/es";
+
+
+    @BeforeClass
+    @Parameters("browser")
+
+    public static void setUp(String browser) { // changed to static
+
+
+        if (browser.equalsIgnoreCase("Chrome"))
+
+        {
+            System.setProperty("webdriver.chrome.driver", "C:/BrowserUtilities/chromedriver.exe");
+            driver = new ChromeDriver();
+
+
+        } else if (browser.equalsIgnoreCase("IE"))
+
+        {
+          /*  DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+            caps.setCapability("ignoreZoomSetting", true);*/
+
+            System.setProperty("webdriver.ie.driver", "C:/BrowserUtilities/IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+
+
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            System.setProperty("webdriver.firefox.marionette", "C:/BrowserUtilities/geckodriver.exe");
+
+            FirefoxDriver driver = new FirefoxDriver();
+
+
+        }
+    }
+
+    @Test
+    public void testAddAnItemToBasket() {
+        driver.get(sURL);
+        driver.manage().window().maximize();
+        HomePage homePage = new HomePage(driver);
+        homePage.clickOnbtnCloseNewsLetter();
+        homePage.clickOnCookieAlert();
+        homePage.clickOnSearchIcon();
+        homePage.enterSearchTextAndSearch("13223971004");
+
+    }
+
+    }
