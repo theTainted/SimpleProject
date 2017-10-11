09ab47de8a8c7a8754a5b12eae7f714c8692ffb7
syam.s@outlook.com <Liverp00l>
2017-10-11 13:37:05 +0200

    Added  in PDP.java @FindBys(@FindBy(xpath="//div[@id='product-content']/ul[@class='swatches c02__size-list size-list']/li")

--- src/test/java/pageobjects/PDP.java
+++ src/test/java/pageobjects/PDP.java
@@ -1,8 +1,26 @@
 package pageobjects;
 
-import org.testng.asserts.SoftAssert;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.support.FindBy;
+        import org.openqa.selenium.support.FindBys;
+        import org.openqa.selenium.support.PageFactory;
+        import org.testng.asserts.SoftAssert;
+
+        import java.util.List;
 
 public class PDP extends Utilities{
     SoftAssert softAssert =new SoftAssert();
+    Utilities utilities = new Utilities();
 
+    @FindBys(@FindBy(xpath="//div[@id='product-content']/ul[@class='swatches c02__size-list size-list']/li"))
+     public   List<WebElement> sizes;
+
+
+
+    public PDP (WebDriver driver) {
+
+        // Utilities.driver=driver; //-> added after making the driver static
+        PageFactory.initElements(driver,this);
+    }
 }
