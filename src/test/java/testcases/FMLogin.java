50bb6f90bf8db86f7455e488ac52f533352be0dd
syam.s@outlook.com <Liverp00l>
2017-10-13 11:09:39 +0200

    FMLogin testcase for webshop

---
+++ src/test/java/testcases/FMLogin.java
@@ -0,0 +1,47 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+public class FMLogin {
+    public static WebDriver driver; // changed to static
+    public String sURL = "https://" + "storefront" + ":" + "storefront" + "@" + "staging.cortefiel.com/es/es";
+
+
+    @BeforeClass
+    @Parameters("browser")
+
+    public static void setUp(String browser) { // changed to static
+
+
+        if (browser.equalsIgnoreCase("Chrome"))
+
+        {
+            System.setProperty("webdriver.chrome.driver", "C:/BrowserUtilities/chromedriver.exe");
+            driver = new ChromeDriver();
+
+
+        } else if (browser.equalsIgnoreCase("IE"))
+
+        {
+          /*  DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+            caps.setCapability("ignoreZoomSetting", true);*/
+
+            System.setProperty("webdriver.ie.driver", "C:/BrowserUtilities/IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+
+
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            System.setProperty("webdriver.firefox.marionette", "C:/BrowserUtilities/geckodriver.exe");
+
+            FirefoxDriver driver = new FirefoxDriver();
+
+
+        }
+    }
+
+}
