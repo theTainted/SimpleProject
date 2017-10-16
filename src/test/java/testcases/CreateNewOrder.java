7a7b974da7d9d8f9a334f57f0ac4a8a71e281b74
syam.s@outlook.com <Liverp00l>
2017-10-16 12:28:39 +0200

    Added CreateNewOrder.Java

---
+++ src/test/java/testcases/CreateNewOrder.java
@@ -0,0 +1,67 @@
+package testcases;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pageobjects.FMLoginPage;
+
+import java.util.List;
+
+public class CreateNewOrder {
+    public static WebDriver driver; // changed to static
+    public String sURL = "http://t2.fleurametz.com";
+
+
+    @BeforeClass
+    @Parameters("browser")
+
+    public static void setUp(String browser) { // changed to static
+
+
+        if (browser.equalsIgnoreCase("Chrome"))
+
+        {
+            System.setProperty("webdriver.chrome.driver", "C:/BrowserUtilities/chromedriver.exe");
+            driver = new ChromeDriver();
+
+
+        } else if (browser.equalsIgnoreCase("IE"))
+
+        {
+          /*  DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+            caps.setCapability("ignoreZoomSetting", true);*/
+
+            System.setProperty("webdriver.ie.driver", "C:/BrowserUtilities/IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+
+
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            System.setProperty("webdriver.firefox.marionette", "C:/BrowserUtilities/geckodriver.exe");
+
+            FirefoxDriver driver = new FirefoxDriver();
+
+
+        }
+    }
+
+    @Test
+    public void testCreateNewOrder(){
+        driver.get(sURL);
+        driver.manage().window().maximize();
+        FMLoginPage fmloginpage = new FMLoginPage(driver);
+        fmloginpage.enterLogin("mobilexl@fleurametz.com");
+        fmloginpage.enterPassword("lelie");
+        fmloginpage.clickLoginButton();
+
+
+    }
+
+
+}
+
+
