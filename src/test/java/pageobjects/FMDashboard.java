4bc9deae9318a909143d9dd68dbdd53f71b5d3c5
syam.s@outlook.com <Liverp00l>
2017-10-20 09:39:06 +0200

    Added   public void clickOnAnAvailableDateInTheCalendar(String sDate) {         for (WebElement test : possibleDeliveryDatesWithNoOrders) {             Date date = new Date();             // System.out.println(date);             if (test.getText().equalsIgnoreCase(sDate)) {                 test.click();                 break;             }         }     }
        public void clickOnOrderButton(String sOrderButtonLabel) {
            for (WebElement newOrder : createNewOrder) {
                if (newOrder.getText().equalsIgnoreCase(sOrderButtonLabel)) {
                   // System.out.println("clickOnOrderButton");
                    newOrder.click();
                    break;
                }
            }
        }
    }

--- src/test/java/pageobjects/FMDashboard.java
+++ src/test/java/pageobjects/FMDashboard.java
@@ -1,5 +1,6 @@
 package pageobjects;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,13 +8,17 @@ import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
 
+import java.util.Date;
 import java.util.List;
 
 public class FMDashboard {
     SoftAssert softAssert = new SoftAssert();
     Utilities utilities = new Utilities();
 
-    @FindBys(@FindBy(xpath ="//div[@id='start-new-order-calendar']//button//span[@data-translate='OrderActions.CreateNewOrder']"))
+    @FindBys(@FindBy(xpath="//div[@id='start-new-order-calendar']//form//table//td[@class='delivery']//a[@class='order-day-link']"))
+    List<WebElement> possibleDeliveryDatesWithNoOrders;
+
+    @FindBys(@FindBy(xpath ="//div[@id='start-new-order-calendar']//button//span[@data-translate]"))
     List<WebElement> createNewOrder;
 
 
@@ -21,11 +26,21 @@ public class FMDashboard {
         PageFactory.initElements(driver, this);
 
     }
+    public void clickOnAnAvailableDateInTheCalendar(String sDate) {
+        for (WebElement test : possibleDeliveryDatesWithNoOrders) {
+            Date date = new Date();
+            // System.out.println(date);
+            if (test.getText().equalsIgnoreCase(sDate)) {
+                test.click();
+                break;
+            }
+        }
+    }
 
     public void clickOnOrderButton(String sOrderButtonLabel) {
         for (WebElement newOrder : createNewOrder) {
             if (newOrder.getText().equalsIgnoreCase(sOrderButtonLabel)) {
-                System.out.println("clickOnOrderButton");
+               // System.out.println("clickOnOrderButton");
                 newOrder.click();
                 break;
             }
