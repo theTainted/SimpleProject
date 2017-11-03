a9961670d0a5da1d6df590a0600688bb57d13089
syam.s@outlook.com <Liverp00l>
2017-11-03 11:23:35 +0100

    add to basket doesnot seem to work

--- src/test/java/testcases/AddAnItemToBasket.java
+++ src/test/java/testcases/AddAnItemToBasket.java
@@ -1,11 +1,13 @@
 package testcases;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -54,15 +56,25 @@ public class AddAnItemToBasket {
     @Test
     public void testAddAnItemToBasket() throws InterruptedException {
         driver.get(sURL);
-        driver.manage().window().maximize();
+            driver.manage().window().maximize();
         HomePage homePage = new HomePage(driver);
         homePage.clickOnbtnCloseNewsLetter();
         homePage.clickOnCookieAlert();
-        homePage.clickOnSearchIcon();
+        Thread.sleep(1000);
+       // driver.findElement(By.xpath("//a[@class='btn-cancel c17__close c17__close--usp icon-close']")).click();
+        homePage.selectCountry(driver,"Esp");
+       homePage.clickOnSearchIcon();
         homePage.enterSearchTextAndSearch("dress");
         Thread.sleep(5000);
-        PDP pdp = new PDP(driver);
+        List <WebElement> plp =driver.findElements(By.xpath("//div/a[@class='c05__thumb-link thumb-link']"));
+        plp.get(0).click();
+
+
+
+       PDP pdp = new PDP(driver);
+       Thread.sleep(3000);
         pdp.clickOnFirstAvailableSize();
+        Thread.sleep(1000);
         pdp.clickAddToBasketButton();
 
 
@@ -74,3 +86,18 @@ public class AddAnItemToBasket {
     }
 
     }
+/* WebElement country = driver.findElement(By.linkText("Netherlands"));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click()", country);
+        driver.findElement(By.cssSelector("span.ui-selectmenu-text")).click();
+        List <WebElement> listOfCountries = driver.findElements(By.xpath("//div[@class='ui-selectmenu-menu ui-front ui-selectmenu-open']//ul[@id='country-selector-menu']/li"));
+        int countryList = driver.findElements(By.xpath("//div[@class='ui-selectmenu-menu ui-front ui-selectmenu-open']//ul[@id='country-selector-menu']/li")).size();
+        System.out.println("CountryListSize : "+countryList);
+        for (WebElement countryName: listOfCountries){
+            System.out.println("Country Name : "+countryName.getText());
+            if (countryName.getText().contains("ESP")){
+                countryName.click();
+                driver.findElement(By.xpath("//button[@class='button button-large button-primary c13__country-selector-button-single']")).click();
+                break;
+            }
+        }*/
