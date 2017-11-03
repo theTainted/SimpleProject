be1b97dcdc7702e1bd2de6cd53d77bd0e35e0cd9
syam.s@outlook.com <Liverp00l>
2017-11-03 11:11:14 +0100

    public void clickOnFirstAvailableSize(){         String firstAvailableSize=sizePresent.get(0).getText();         System.out.println(firstAvailableSize);         sizePresent.get(0).click();

--- src/test/java/pageobjects/PDP.java
+++ src/test/java/pageobjects/PDP.java
@@ -13,7 +13,7 @@ public class PDP extends Utilities{
     SoftAssert softAssert =new SoftAssert();
     Utilities utilities = new Utilities();
 
-    @FindBys(@FindBy(xpath="//div[@id='product-content']//div[@class='c02__sizes']//ul[@class='swatches c02__size-list size-list']//a"))
+    @FindBys(@FindBy(xpath="//div[@class='c02__sizes']//ul[@class='swatches c02__size-list size-list']//li[@class='c02__size-item swatch-item available']//a"))
      public   List<WebElement> sizePresent;
     @FindBy(id="add-to-cart")
     public WebElement btnAddToBasket;
@@ -26,18 +26,10 @@ public class PDP extends Utilities{
         PageFactory.initElements(driver,this);
     }
     public void clickOnFirstAvailableSize(){
-        for (int i=0;i<sizePresent.size();i++){
+        String firstAvailableSize=sizePresent.get(0).getText();
+        System.out.println(firstAvailableSize);
+        sizePresent.get(0).click();
 
-            String size =sizePresent.get(i).getText();
-            String titleSize =sizePresent.get(i).getAttribute("title");
-            if(titleSize.toLowerCase().indexOf("not available") == -1){
-
-                sizePresent.get(i).click();
-                System.out.println(sizePresent.get(i).getText());
-                break;
-            }
-
-        }
     }
     public void clickAddToBasketButton(){
         btnAddToBasket.click();
