d6cb234247ce41fee4c63319930b9540378dae2f
syam.s@outlook.com <Liverp00l>
2017-11-22 15:11:48 +0100

    nothing updated

--- src/test/java/pageobjects/PDP.java
+++ src/test/java/pageobjects/PDP.java
@@ -9,36 +9,36 @@ package pageobjects;
 
         import java.util.List;
 
-public class PDP extends Utilities {
-    SoftAssert softAssert = new SoftAssert();
+public class PDP extends Utilities{
+    SoftAssert softAssert =new SoftAssert();
     Utilities utilities = new Utilities();
 
-    @FindBys(@FindBy(xpath = "//div[@class='c02__sizes']//ul[@class='swatches c02__size-list size-list']//li[@class='c02__size-item swatch-item available']//a"))
-    public List<WebElement> sizePresent;
-    @FindBy(xpath = "//button[@id='add-to-cart']")
+    @FindBys(@FindBy(xpath="//div[@class='c02__sizes']//ul[@class='swatches c02__size-list size-list']//li[@class='c02__size-item swatch-item available']//a"))
+     public   List<WebElement> sizePresent;
+    @FindBy(xpath="//button[@id='add-to-cart']")
     public WebElement btnAddToBasket;
-    @FindBys(@FindBy(xpath = "//span[@class='c15-icon-link__icon icon-bag']"))
+    @FindBys(@FindBy(xpath="//span[@class='c15-icon-link__icon icon-bag']"))
     public List<WebElement> miniBasketIcon;
 
 
-    public PDP(WebDriver driver) {
+
+
+    public PDP (WebDriver driver) {
 
         // Utilities.driver=driver; //-> added after making the driver static
-        PageFactory.initElements(driver, this);
+        PageFactory.initElements(driver,this);
     }
-
-    public void clickOnFirstAvailableSize() {
-        String firstAvailableSize = sizePresent.get(0).getText();
+    public void clickOnFirstAvailableSize(){
+        String firstAvailableSize=sizePresent.get(0).getText();
         sizePresent.get(0).click();
 
     }
-
-    public void clickAddToBasketButton() {
+    public void clickAddToBasketButton(){
         System.out.println(btnAddToBasket.isDisplayed());
         btnAddToBasket.click();
     }
-
-    public void clickOnMiniBasketIcon() {
+    public void clickOnMiniBasketIcon(){
         miniBasketIcon.get(0).click();
     }
+
 }
