0fce76e287d1695c5a46597e8aae455cfcb93b88
syam.s@outlook.com <Liverp00l>
2017-11-22 15:08:21 +0100

    @FindBys(@FindBy(xpath="//span[@class='control radio']//label[@class='control-label']")) public List<WebElement> labelsShippingMethods;
     public void clickOnShipmentMethods(String partialShippingMethodLabel){
            for(WebElement shippingMethod:labelsShippingMethods){
                System.out.println(shippingMethod.getText());
                if(shippingMethod.getText().contains("Recoger ")){
                    shippingMethod.click();
                    break;
                }

--- src/test/java/pageobjects/ShoppingBasket.java
+++ src/test/java/pageobjects/ShoppingBasket.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
+
+import java.util.List;
 
 public class ShoppingBasket {
     SoftAssert softAssert =new SoftAssert();
@@ -13,7 +16,8 @@ public class ShoppingBasket {
 
 @FindBy(xpath="//form[@id='checkout-form']//button[@class='button button-large button-primary button-block']")
 public WebElement btnContinue;
-
+@FindBys(@FindBy(xpath="//span[@class='control radio']//label[@class='control-label']"))
+public List<WebElement> labelsShippingMethods;
 
     public  ShoppingBasket(WebDriver driver){
         PageFactory.initElements(driver,this);
@@ -22,5 +26,14 @@ public WebElement btnContinue;
 
     public void clickOnContinueButton(){
         btnContinue.click();
+    }
+
+    public void clickOnShipmentMethods(String partialShippingMethodLabel){
+        for(WebElement shippingMethod:labelsShippingMethods){
+            System.out.println(shippingMethod.getText());
+            if(shippingMethod.getText().contains("Recoger ")){
+                shippingMethod.click();
+                break;
+            }
     }
 }
